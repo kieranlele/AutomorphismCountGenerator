d0f4b171a04360c5ba2c2ff74ab380619771323c
Fayfay Ning <fayfayning@Fayfays-MBP.fios-router.home>
2021-06-04 03:08:45 -0400

    smoother, better counting

--- automorph_calc.py
+++ automorph_calc.py
@@ -5,6 +5,7 @@ Created on 6/3/21
 """
 import math
 
+
 # checks if L1 and L2 are equal
 def checkL1L2(tree):
     ind_1 = tree.index(1)
@@ -16,17 +17,15 @@ def checkL1L2(tree):
         return True
     return False
 
+
 # Gets the label of the rth node of the tree
 def get_label(r, tree):
     lr = tree[r]
-    last_step = True
+    r1 = len(tree)
     for i in range(r + 1, len(tree)):
         if tree[i] <= lr:
             r1 = i - 1
-            last_step = False
             break
-    if last_step:
-        r1 = len(tree)
     for j in range(r - 1, -1, -1):
         if tree[j] == lr - 1:
             r2 = j + 1
@@ -35,32 +34,29 @@ def get_label(r, tree):
     label.extend(tree[r:r1 + 1])
     return label
 
+
 # Iterates through the tree to get all the labels
 def all_labels(tree):
     labels = []
-    #print(get_label(10, tree))
     for r in range(1, len(tree)):
         label = get_label(r, tree)
         labels.append(label)
     return labels
 
+
 # Calculates the number of automorphisms using the labels
 def calc_aut(labels):
-    labels.sort()
-    label = labels[0]
-    indexes = []
-    count = 0
-    for i in range(1, len(labels)):
-        if label != labels[i]:
-            indexes.append(i - count)
-            count = i
-            label = labels[i]
-    indexes.append(len(labels) - count)
+    dict = {}
     num = 1
-    for i in indexes:
-        if i != 1:
-            num *= math.factorial(i)
+    for i in labels:
+        tup = tuple(i)
+        if tup not in dict:
+            dict[tup] = 0
+        dict[tup] += 1
+    for i in dict.values():
+        num *= math.factorial(i)
     return num
+
 
 # Runs program on a given tree
 def aut(tree):
@@ -75,36 +71,8 @@ def aut(tree):
         num = aut_L1 * aut_L1 * 2
     return num
 
+
 if __name__ == '__main__':
     tree = [0, 1, 2, 3, 3, 3, 2, 1, 2, 2, 1, 2, 2]
     fin = aut(tree)
     print("fin", fin)
-
-    """
-    for i in range(r + 1, len(tree)):
-        if (tree.index())
-        if tree[i] == lr:
-            r1 = i - 1
-            break
-
-    try:
-        r1 = tree.index(lr, r + 1) - 1
-        try:
-            r1_1 = tree.index(lr - 1, r + 1) - 1
-            r1 = min(r1, r1_1)
-        except ValueError:
-            print(2)
-            pass
-    except ValueError:
-        r1 = r
-
-    try:
-        r1_1 = tree.index(lr, r + 1) - 1
-    except ValueError:
-        r1_1 = len(tree) + 1
-    try:
-        r1_2 = tree.index(lr - 1, r + 1) - 1
-    except ValueError:
-        r1_2 = len(tree) + 1
-    r1 = min(r1_1, r1_2)
-    """
